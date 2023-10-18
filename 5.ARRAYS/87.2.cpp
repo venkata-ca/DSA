c37882e2826bb2807ef5d7e3f57c4ae3396aeacd
durgaprasadbvdp <durgaprasadbvdp@gmail.com>
2023-10-19 00:12:27 +0530

    Arrays..

--- 5.ARRAYS/87.2.cpp
+++ 5.ARRAYS/87.2.cpp
@@ -1,5 +1,5 @@
 /*
-87.1Stock buy and sell problem.
+87.2.Stock buy and sell problem.
 
     You are given an array of prices where prices[i] is the price of a given stock on an ith day.
     You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
